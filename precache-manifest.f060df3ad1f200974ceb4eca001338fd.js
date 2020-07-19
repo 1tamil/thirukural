a27001264c2275f8ab355a97beef9c61be60f894
1tamil <tzrajesh7@outlook.com>
2020-07-19 07:14:02 +0000

    new deploy

---
+++ precache-manifest.f060df3ad1f200974ceb4eca001338fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59eacc77cfffc1f9c9f0d506a3b49497",
+    "url": "/thirukural/index.html"
+  },
+  {
+    "revision": "e2789dfc40a56deb41a9",
+    "url": "/thirukural/static/css/main.29aadb6c.chunk.css"
+  },
+  {
+    "revision": "873010a2c23e911e263e",
+    "url": "/thirukural/static/js/2.1fb9fc17.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/thirukural/static/js/2.1fb9fc17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2789dfc40a56deb41a9",
+    "url": "/thirukural/static/js/main.2a721971.chunk.js"
+  },
+  {
+    "revision": "84badf721e6565f407a8",
+    "url": "/thirukural/static/js/runtime-main.9b95ef5c.js"
+  },
+  {
+    "revision": "91151f9d3a1b2eafe65836fe9b9e08dd",
+    "url": "/thirukural/static/media/thirukural.91151f9d.png"
+  }
+]);
